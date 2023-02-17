1909e9eedd24e4f024f610fa5f97ecbc9f94589b
Grizzly18 <mz-100@mail.ru>
2023-02-17 23:52:15 +0300

    gg

--- main.py
+++ main.py
@@ -14,18 +14,55 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 conn = sqlite3.connect('info.db') # Потом поменяем
 sql = conn.cursor()
 
+class Queue(StatesGroup):
+    first = State()
+    second = State()
+    third = State()
+
+def kb():
+    return ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+def bt(message):
+    return KeyboardButton(message)
+
 def start_kb():
-    button_reg = KeyboardButton('Хз1')
-    button_enter = KeyboardButton('Тоже хз')
-    st_kn = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button_reg, button_enter)
+    button_1 = bt('Учебные программы')
+    button_2 = bt('Проживание')
+    button_3 = bt('Приемная кампания')
+    button_4 = bt('Другое')
+    st_kn = kb().add(button_1, button_2, button_3, button_4)
     return st_kn
 
+async def g(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Введите логин')
+
+@dp.message_handler(text=['Вход'], state=None)
+async def bot_enter(message: types.Message):
+    g(message)
 
 # Старт бота
 @dp.message_handler(commands=['start'], state=None)
 async def bot_start(message: types.Message):
-    await bot.send_message(message.from_user.id, "Здравствуйте! \n"
-                                                 "Я бот", reply_markup=start_kb())
+    await bot.send_message(message.from_user.id, """Здравствуйте!\n
+                           Это телеграм-бот для интересующихся поступлением в университет Innopolis абитуриентов.\n
+                           Вам нужно выбрать несколько категорий.\nЧто бы вы хотели узнать?""", reply_markup=start_kb())
+    await Queue.first.set()
+
+def HandlerFirst(message):
+    answers = {
+        "учебные программы": ("Пук-Хрюк", kb().add(bt('Бакалавриат'), bt('Магистратура'), bt('Аспирантура'))),
+        "поступление": ("Пук-Хрюк", 
+                        kb().add(bt('День открытых дверей'), bt('Подать заявку'), bt('Отбор'), 
+                        bt('Платное обучение'), bt('Грант'), bt('Олимпиадные бонусы'), bt('Документы'))),
+        "проживание": ("Пук-Хрюк", kb().add(bt('Кампус университета Иннополис'), bt('Студенческая жизнь'), bt('Город Иннополис'))),
+        "другое": ("Пук-Хрюк", kb().add(bt('Отзывы'), bt('Новости и события'), bt('Часто задаваемые вопросы'), bt('Контакты'), bt('Статистика за 5 лет'))),
+    }
+    return answers[message.lower()]
+
+@dp.message_handler(state=Queue.first)
+async def what_edit_profile(message: types.Message, state: FSMContext):
+    handler = HandlerFirst(message.text)
+    await bot.send_message(message.from_user.id, handler[0], reply_markup=handler[1])
 
 
 if __name__ == '__main__':
